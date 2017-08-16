66a7c1573d25966977b169cbb02945f91cc02ab7
SmileMylife <843833414@qq.com>
2017-08-16 16:17:40 +0800

    no summary

---
+++ bos_management/src/main/java/com/baidu/dao/es/WayBillElasticsearchRepository.java
@@ -0,0 +1,11 @@
+/*package com.baidu.dao.es;
+
+import com.baidu.domain.WayBill;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+*//**
+ * Created by ZhangPei on 2017/8/13.
+ *//*
+public interface WayBillElasticsearchRepository extends ElasticsearchRepository<WayBill,Integer> {
+
+}*/
